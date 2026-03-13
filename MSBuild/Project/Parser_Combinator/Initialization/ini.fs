46b0b86994a2cdba9a432d249ec6cf431155357b
eristocrates <reillocb@gmail.com>
2026-03-13 16:20:25 -0400

    successful ini parser combinator

--- MSBuild/Project/Parser_Combinator/Initialization/ini.fs
+++ MSBuild/Project/Parser_Combinator/Initialization/ini.fs
@@ -17,13 +17,61 @@ open Parser_Combinator
 open Computer_Storage
 
 
+type User_State'Type = unit
 
 module comment =
-    type Comment = Comment of sigil: string * comment: string
+    let parser: Parser<string, User_State'Type> =
+        pstring ";" >>. spaces >>. restOfLine true
 
-    let parser: Parser<Comment, unit> = pstring ";" .>>. restOfLine true |>> Comment
+module value =
+    type Value'Type = Value of string
+
+module key =
+    module name =
+        let parser: Parser<string, unit> =
+            many1Satisfy (fun character ->
+                not (System.Char.IsWhiteSpace character)
+                && character <> '=')
+
+    let parser: Parser<string * string, unit> =
+        name.parser .>> spaces .>> pstring "=" .>> spaces
+        .>>. manySatisfy (fun character -> not (character = '\r') && not (character = '\n'))
+
+module section =
+    module name =
+        let parser: Parser<string, User_State'Type> =
+            pstring "["
+            >>. manySatisfy (fun character -> not (character = ']'))
+            .>> pstring "]"
+            .>> newline
+
+
+
+
 
 module ini =
+    type Ini'Token =
+        | White_Space of unit
+        | Comment of comment: string
+        | Key_Name of string
+        | Value of string
+        | Key of key_name: string * value: string
+        | Section_Name of string
+        | Section of section_name: string * key_name: string * equal_sign: string * value: string
+
+    let parser: Parser<Ini'Token list, User_State'Type> =
+        spaces
+        >>. manyTill
+                (choice [
+
+                          spaces1 |>> White_Space
+                          comment.parser |>> Comment
+                          section.name.parser |>> Section_Name
+                          key.parser |>> Key
+
+                           ])
+                eof
+
     let ini =
         {
 
@@ -33,8 +81,24 @@ module ini =
 
 let test_file = Input_Stream.file ini.ini
 
-let test_string =
-    """; last modified 1 April 2001 by John Doe
-"""
 
-let test_result = apply_parser comment.parser test_file ()
+let test_result = apply_parser ini.parser test_file ()
+
+let keys =
+    test_result.output.Value
+    |> List.filter (fun token -> token.IsKey)
+
+let urlToken =
+    keys
+    |> List.filter (fun token ->
+        match token with
+        | ini.Ini'Token.Key (name, value) -> name = "url")
+
+    |> List.head
+
+let url =
+    match urlToken with
+    | ini.Ini'Token.Key (name, value) -> value
+
+// let test_string = Input_Stream.string "name = John Doe"
+// apply_parser key.parser test_string ()
