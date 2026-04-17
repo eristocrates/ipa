d5e55eee26617db1319c6c5c2a7bd89bb05daa74
eristocrates <reillocb@gmail.com>
2026-04-16 23:59:57 -0400

    hard learned but valuable lessons on unicode

---
+++ Source-code/Host-environment/Common-Language-Runtime/FSharp/Project/Ergonomics/XParsecErgonomics.fsx
@@ -0,0 +1,76 @@
+﻿open System
+open System.Text
+open System.IO
+open System.Globalization
+
+#r "nuget: XParsec"
+
+open XParsec
+open XParsec.Parsers
+open System.Collections.Immutable
+
+
+
+
+#load @"C:\Repositories\eristocrates\ipa\Source-code\Host-environment\Common-Language-Runtime\FSharp\Project\UnicodeStandard\UnicodeStandard.fsx"
+open UnicodeStandard
+
+
+type Adposition = | OnInput
+
+let any_unicodepoint (input: string) =
+    unicodepoints'from'string input |> anyOf
+
+let parse_unicodepoint (input: string) =
+    unicodepoints'from'string input
+    |> Array.map (fun unicodepoint -> pitem unicodepoint)
+
+let skip_unicodepoint (input: string) =
+    unicodepoints'from'string input
+    |> Array.map (fun unicodepoint -> skipItem unicodepoint)
+
+
+let parse_expecting parse (expecting: string) = parse <??> expecting
+
+
+
+
+let run_partial_parse parse (adposition: Adposition) (input: string) =
+    let unicodepoints = unicodepoints'from'string input
+    let text = Reader.ofArray unicodepoints ()
+    parse text
+
+let run_full_parse parse (adposition: Adposition) (input: string) =
+    let unicodepoints = unicodepoints'from'string input
+    let text = Reader.ofArray unicodepoints ()
+    (parse .>> eof) text
+
+let result_from_parse parse (adposition: Adposition) (input: string) =
+    let unicodepoints = unicodepoints'from'string input
+    let text = Reader.ofArray unicodepoints ()
+    let (Result.Ok result) = (parse .>> eof) text
+    result
+
+let error_from_parse parse (adposition: Adposition) (input: string) =
+    let unicodepoints = unicodepoints'from'string input
+    let text = Reader.ofArray unicodepoints ()
+    let (Result.Error result) = (parse .>> eof) text
+    result
+
+
+let int_from_stringNumeral (stringNumeral: string) =
+    match System.Int32.TryParse(stringNumeral) with
+    | true, intNumeral -> intNumeral
+    | _ -> failwithf "Invalid numeral string: %s" stringNumeral
+
+let intNumeral_from_RuneNumeral (RuneNumeral: Rune) =
+    int_from_stringNumeral (string RuneNumeral)
+
+let int_from_singleDigit (singleDigit: Rune) =
+    int_from_stringNumeral (string singleDigit)
+
+let int_from_doubleDigit (leftDigit: Rune) (rightDigit: Rune) =
+    int_from_stringNumeral $"{leftDigit}{rightDigit}"
+
+let int_from_tripleDigit (leftDigit: Rune) (centerDigit: Rune) (rightDigit: Rune) =
+    int_from_stringNumeral $"{leftDigit}{centerDigit}{rightDigit}"
---
+++ Source-code/Host-environment/Common-Language-Runtime/FSharp/Project/UnicodeStandard/UnicodeStandard.fsx
@@ -0,0 +1,212 @@
+﻿open System
+open System.Linq
+open System.Text
+open System.Text.Unicode
+open System.Globalization
+open System.Xml.Linq
+
+#r "nuget: FSharp.Data"
+open FSharp.Data
+
+
+module ucd =
+    module flat =
+        [<Literal>]
+        let flatFilePath =
+            @"D:\Surface\Standards\Unicode\UCD\17.0.0\ucdxml\ucd.all.flat.xml"
+
+        let xml = XmlProvider<flatFilePath>.Load flatFilePath
+
+    module grouped =
+        [<Literal>]
+        let groupedFilePath =
+            @"D:\Surface\Standards\Unicode\UCD\17.0.0\ucdxml\ucd.all.grouped.xml"
+
+        let xml = XmlProvider<groupedFilePath>.Load groupedFilePath
+
+
+
+[<Struct>]
+type Unicodepoint = { value: int }
+
+[<Struct>]
+type Grapheme_Cluster = { as_string: string }
+
+[<Struct>]
+[<RequireQualifiedAccess>]
+type Unicode_Alias_Type =
+    | abbreviation of string
+    | control of string
+    | figment of string
+    | correction of string
+    | alternate of string
+    | unknown of string
+    static member from_string(alias_type: string) =
+        match alias_type with
+        | "abbreviation" -> abbreviation alias_type
+        | "control" -> control alias_type
+        | "figment" -> figment alias_type
+        | "correction" -> correction alias_type
+        | "alternate" -> alternate alias_type
+        | _ -> unknown alias_type
+
+
+[<Struct>]
+type Unicode_Alias =
+    {
+
+      alias: string
+      alias_type: Unicode_Alias_Type
+
+     }
+
+[<Struct>]
+type Unicode_Name =
+    {
+
+      current: string option
+      version1: string option
+      aliases: Unicode_Alias array
+
+     }
+
+[<Struct>]
+type Unicode_Block =
+    {
+
+      name: string
+      from_codepoint_value: int
+      to_codepoint_value: int
+
+     }
+
+[<Struct>]
+type Unicode_Named_Sequence =
+    {
+
+      name: string
+      codepoint_values: int array
+
+     }
+
+
+let unicodepoint_value'from'ucd_string (ucd_string: string) = int $"0x{ucd_string}"
+
+
+
+
+let grapheme_clusters'from'string (input: string) =
+    seq {
+        let enumerator = StringInfo.GetTextElementEnumerator(input)
+
+        while enumerator.MoveNext() do
+            yield { as_string = enumerator.GetTextElement() }
+
+
+
+    }
+    |> Seq.toArray
+
+let unicodepoints'from'grapheme_cluster (grapheme_cluster: Grapheme_Cluster) =
+    grapheme_cluster.as_string.EnumerateRunes()
+    |> Seq.map (fun rune -> { value = rune.Value })
+    |> Seq.toArray
+
+
+let unicodepoints'from'string (input: string) =
+    let grapheme_clusters = grapheme_clusters'from'string input
+
+    grapheme_clusters
+    |> Array.collect (fun grapheme_cluster -> unicodepoints'from'grapheme_cluster grapheme_cluster)
+
+
+
+let string'from'unicodepoint (unicodepoint: Unicodepoint) = Rune(unicodepoint.value).ToString()
+
+let name'from'unicodepoint (unicodepoint: Unicodepoint) =
+    let unicode_char =
+        ucd.flat.xml.Repertoire.Chars
+        |> Array.find (fun unicode_char -> unicodepoint_value'from'ucd_string unicode_char.Cp.Value = unicodepoint.value)
+
+    let aliases =
+        unicode_char.NameAlias
+        |> Array.map (fun name_alias ->
+
+            {
+
+              alias = name_alias.Alias
+              alias_type = Unicode_Alias_Type.from_string name_alias.Type
+
+            }
+
+        )
+
+    {
+
+      current = unicode_char.Na
+      version1 = unicode_char.Na1
+      aliases = aliases
+
+    }
+
+let block'from'unicodepoint (unicodepoint: Unicodepoint) =
+    let unicode_block =
+        ucd.grouped.xml.Blocks
+        |> Array.find (fun unicode_block ->
+
+            unicodepoint_value'from'ucd_string unicode_block.FirstCp.Value
+            <= unicodepoint.value
+            && unicodepoint.value
+               <= unicodepoint_value'from'ucd_string unicode_block.LastCp
+
+        )
+
+
+    {
+
+      name = unicode_block.Name
+      from_codepoint_value = unicodepoint_value'from'ucd_string unicode_block.FirstCp.Value
+      to_codepoint_value = unicodepoint_value'from'ucd_string unicode_block.LastCp
+
+    }
+
+
+
+let named_sequences'from'unicodepoint (unicodepoint: Unicodepoint) =
+
+    let named_sequences =
+        ucd.grouped.xml.NamedSequences
+        |> Array.choose (fun sequence ->
+            let cps =
+                sequence.Cps.Split(" ")
+                |> Array.map (fun ucd_string -> unicodepoint_value'from'ucd_string ucd_string)
+
+
+
+
+            if cps.Contains(unicodepoint.value) then
+
+                Some(
+                    {
+
+                      name = sequence.Name
+                      codepoint_values = cps
+
+                    }
+                )
+            else
+                None
+
+        )
+
+    named_sequences
+
+
+(*
+let test_string = grapheme_clusters'from'string "test"
+let test_codepoint = test_string[0].as_codepoints[0]
+name'from'unicodepoint test_codepoint
+block'from'unicodepoint test_codepoint
+named_sequences'from'unicodepoint test_codepoint
+
+*)
