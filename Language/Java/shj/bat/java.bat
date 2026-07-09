b0832411001d66430f4ef3a985074cc0aa906915
eristocrates <reillocb@gmail.com>
2026-07-09 12:14:11 -0400

    readd java

---
+++ Language/Java/shj/bat/java.bat
@@ -0,0 +1,5 @@
+@echo off
+REM === Forward Java call through cmd.exe ===
+REM echo Intercepted Java call: %*
+REM cmd /c ""C:\Users\CollierB\.shbin\shj\bin\java.exe" %*"
+cmd /c ""C:\Users\CollierB\.shbin\shj\bin\java.exe" %*"
